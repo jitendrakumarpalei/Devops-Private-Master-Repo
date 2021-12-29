8bfd3e2697a0f97ad1894f1acf4eaeba4fed77f3
AshantaP <59921272+AshantaP@users.noreply.github.com>
2021-12-29 14:04:03 +0530

    Create service-start.sh

---
+++ service-start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ssh -tt jenkins@192.168.1.104
+cd /
+ls -la
+cd /shell-script
+./mysql.sh
+
+=================================================================
+#!/bin/bash
+
+service httpd status | grep 'active (running)' > /dev/null 2>&1
+
+if [ $? != 0 ]
+then
+        sudo service httpd restart > /dev/null
+fi
