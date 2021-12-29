269eba77cd64ef86ad8ad72daf274ccc643f3fd4
AshantaP <59921272+AshantaP@users.noreply.github.com>
2021-12-29 14:30:55 +0530

    Update service-start.sh

--- service-start.sh
+++ service-start.sh
@@ -15,3 +15,15 @@ if [ $? != 0 ]
 then
         sudo service httpd restart > /dev/null
 fi
+===============Final Script=======================================
+#!/bin/bash
+
+ssh -tt jenkins@192.168.1.104
+cd /
+ls -la
+service httpd status | grep 'active (running)' > /dev/null 2>&1
+
+if [ $? != 0 ]
+then
+        sudo service httpd restart > /dev/null
+fi
